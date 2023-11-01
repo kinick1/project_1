01f49e14e14142b7b645dc6d8d74c3d44fb8eb6a
kinick <kimjun9809@gmail.com>
2023-11-01 18:23:46 +0900

    상품 데이터 입력 화면 ui, 등록버튼(데이터 전송은 아직 미구현)

--- app/src/main/java/kr/ac/wku/project_1/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/project_1/MainActivity.kt
@@ -2,17 +2,38 @@ package kr.ac.wku.project_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
+import kr.ac.wku.project_1.data.product
 import kr.ac.wku.project_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    private lateinit var editTextProductName: EditText
+    private lateinit var editTextPurchaseDate: EditText
+    private lateinit var editTextExpirationDate: EditText
+    private lateinit var buttonSave: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        editTextProductName = findViewById(R.id.edtProductName)
+        editTextPurchaseDate = findViewById(R.id.edtPurchaseDate)
+        editTextExpirationDate = findViewById(R.id.edtExpirationDate)
+        buttonSave = findViewById(R.id.btnSave)
 
+        buttonSave.setOnClickListener {
+            saveProduct()
+        }
     }
+        private fun saveProduct() {
+            val productName = editTextProductName.text.toString()
+            val purchaseDate = editTextPurchaseDate.text.toString()
+            val expirationDate = editTextExpirationDate.text.toString()
+
+            val product = product(productName, purchaseDate, expirationDate)
+        }
 }
---
+++ app/src/main/java/kr/ac/wku/project_1/data/product.kt
@@ -0,0 +1,9 @@
+package kr.ac.wku.project_1.data
+
+class product(
+    val name: String,
+    val purchaseDate: String,
+    val expirationDate: String
+) {
+
+}
